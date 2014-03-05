d72d948f82b91f7a1cdb6c824e0be74039bd5866
Richard Burcher <richardburcher@gmail.com>
2014-03-05 11:47:15 -0500

    initial commit of testing suite

--- interactive_tool/StandAloneSoilTool/src/aafc_test.py
+++ interactive_tool/StandAloneSoilTool/src/aafc_test.py
@@ -1,6 +1,24 @@
 """
 test suite
 """
+import os
+import sys
+import sqlite3
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
+
+# TODO: remove from final implementation. not an issue as qgis put .qgis/python/plugins onto sys path
+sys.path.append(r"/Users/drownedfrog/Documents/workspace/git/aafc-grip-tool-dev-dec2013-mar2014/interactive_tool/StandAloneSoilTool/src/")
+
+import aafc_io as inout
+import aafc_utilities as utilities
+import aafc_database as database
+import aafc_gui as gui
+
 
 #TODO: test suite -- code
 
@@ -8,3 +26,156 @@ test suite
 # 974040 -- exact tie
 # 972018 -- 3 values
 # 615009
+
+
+# root directories
+dbfDirectory = r"/Users/drownedfrog/Projects/Contracts/AAFC/dec2013_mar2014_tool_dev/data/Soil/SLC-fordistribution"
+outDirectory = r"/Users/drownedfrog/Projects/Contracts/AAFC/dec2013_mar2014_tool_dev/data/test"
+
+
+# == input paths to dbf's
+# cmp dbf to be converted. * must always be passed if not then quit!
+cmpDbfPath = os.path.join(dbfDirectory,"cmp32.dbf")
+# snf dbf. landuse table.
+snfDbfPath = os.path.join(dbfDirectory,"snf32.dbf")
+# slf dbf. layer table.
+slfDbfPath = os.path.join(dbfDirectory,"slf32.dbf")
+
+
+# == output main db name & path
+# output spatialite db. ogr will add extension.
+sqliteDbName = "soilDb_testSuite"
+sqliteDbPath = os.path.join(outDirectory, sqliteDbName)
+
+
+# =========== configuration options.
+
+# == db fields
+"""
+field names used for linking; primary/foreign key names
+"""
+# what is field name in cmp table to define slc id
+dbSlcIdKey = "sl"
+
+# what field defines soilkey
+dbSoilKey = "soilkey"
+
+# what field reflects percent in cmp
+dbPercentKey = "percent"
+
+# what field in cmp table is cmp field. integer field listing unique id
+dbCmpKey = "cmp"
+
+# what field in snl table is layer_no field.
+dbLayerNumberKey = "layer_no"
+
+
+# == csv prefix name for file to be created
+"""
+default will be calculated
+csv name will be prefixed by user supplied name. remainder of name derived from column
+"""
+csvFilePrefix = "test"
+
+# == snf landuse preference
+"""
+either N or A
+default if not A is N
+"""
+landusePreference = "A"
+
+
+# ========= set high level variables
+# full path to spatialite db
+inSoilDbPath = os.path.join(sqliteDbPath, sqliteDbName + ".sqlite")
+
+
+# ==================================== work flow
+# create utility class instance
+utils = utilities.Utils(iface)
+
+# get path to temp directory
+tempSystemDirectoryPath = utils.determineSystemTempDirectory()
+
+# class instance of io
+io = inout.Io(inSoilDbPath, tempSystemDirectoryPath)
+
+# validate user input
+utils.validateUserInput(cmpDbfPath)
+
+# inform user that db creation is about to start
+utils.communicateWithUserInQgis("Creating new db...",level="INFO", messageExistanceDuration=4)    
+
+# remove existing db if user provides same name
+utils.deleteFile(os.path.join(sqliteDbPath, sqliteDbName))
+
+# remove all layers in qgis
+utils.removeAllQgisLayers()
+
+#=== create db and load with passed dbf paths
+# create new db
+io.createNewDb(cmpDbfPath,snfDbfPath,slfDbfPath)
+
+# create database class instance
+# db must exist before sqlite connection can exit
+db = database.Db(inSoilDbPath, tempSystemDirectoryPath)
+
+# change initial loaded table to correct table name
+# created db table has name of db. change this to name of dbf
+# TODO: parameterize name from dbf -- might need to be generic in utilities. io.createDb has similar
+db.updateDbTableName("cmp32")
+
+# get listing of tables
+results = db.executeSql("select name from sqlite_master where type='table'")
+#print results
+
+#===== demo to stdout
+db.demoCalcCategorical()
+db.demoCalcNumeric()
+
+
+#======== calculations
+#TODO: 1 critical --- parameterize all method arguments below
+
+#===== process single column categorical
+# write all sl's for single column to csv
+# warn user process may take several minutes
+message = "Calculating column %s may take several minutes" % ("slope")
+utils.communicateWithUserInQgis(message,messageExistanceDuration=10)
+# get all distinct id's from cmp table
+ids = db.executeSql("select distinct(sl) from cmp32")
+# convert sl ids list of tuples to simple list
+ids_cleaned = utils.convertDbResults2SimpleList(ids)
+headers, results = db.calculateField(ids_cleaned[:5], dbSlcKey=dbSlcIdKey, tableName="cmp32", column="slope", dbPercentKey=dbPercentKey)
+io.writeCsvFile("slope", headers, results, outDirectory, csvFilePrefixName=csvFilePrefix)
+
+
+#===== process single column numeric
+# write all sl's for single column to csv
+# warn user process may take several minutes
+message = "Calculating column %s may take several minutes" % ("awhc_v")
+utils.communicateWithUserInQgis(message,messageExistanceDuration=10)
+# get all distinct id's from cmp table
+ids = db.executeSql("select distinct(sl) from cmp32")
+# convert sl ids list of tuples to simple list
+ids_cleaned = utils.convertDbResults2SimpleList(ids)
+headers, results = db.calculateField(ids_cleaned[:5], dbSlcKey=dbSlcIdKey, tableName="cmp32", column="awhc_v", dbPercentKey=dbPercentKey)
+io.writeCsvFile("awhc_v", headers, results, outDirectory, csvFilePrefixName=csvFilePrefix)
+
+
+#===== join cmp to snf tables
+db.resultsTableJoiningCmpSnfBySoilkey([242025,376001,615009], dbSlcKey=dbSlcIdKey, dbCmpKey=dbCmpKey, dbSoilKey=dbSoilKey, cmpTableName="cmp32", snfTableName="snf32", landuse=landusePreference)
+
+# categorical calc on joined column g_group3; snf column
+message = "Calculating column %s may take several minutes" % ("g_group3")
+utils.communicateWithUserInQgis(message,messageExistanceDuration=10)
+# get all distinct id's from cmp table
+ids = db.executeSql("select distinct(sl) from results_joinedCmpSnf")
+# convert sl ids list of tuples to simple list
+ids_cleaned = utils.convertDbResults2SimpleList(ids)
+headers, results = db.calculateField(ids_cleaned[:5], dbSlcKey=dbSlcIdKey, tableName="results_joinedCmpSnf", column="g_group3", dbPercentKey=dbPercentKey)
+io.writeCsvFile("'g_group3:1'", headers, results, outDirectory, csvFilePrefixName=csvFilePrefix)
+
+
+#====== join all 3 tables together
+db.resultsTableJoiningCmpSnfSlfBySoilkey([242025,376001,615009], dbSlcKey=dbSlcIdKey, dbCmpKey=dbCmpKey, dbSoilKey=dbSoilKey, dbLayerNumberKey=dbLayerNumberKey, cmpTableName="cmp32", snfTableName="snf32", slfTableName="slf32", landuse=landusePreference, layerNumber=1)
