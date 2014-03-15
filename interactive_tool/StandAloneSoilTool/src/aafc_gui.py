91410d15a09db0d28c7ea46c2c9bf3a6f699b9cc
Richard Burcher <richardburcher@gmail.com>
2014-03-15 15:21:30 -0400

    several items
    remove self reference for testing.
    add qgis module imports.

--- interactive_tool/StandAloneSoilTool/src/aafc_gui.py
+++ interactive_tool/StandAloneSoilTool/src/aafc_gui.py
@@ -4,9 +4,12 @@ gui related tasks.
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
+from qgis.core import *
+from qgis.gui import *
+from qgis.utils import *
 
 
-def getFilePathDialog(self, searchDirectory, filter="dBASE (*.dbf)"):
+def getFilePathDialog(searchDirectory, filter="dBASE (*.dbf)"):
         """
         present standard qt file dialog. user selects single file.
 
@@ -16,13 +19,13 @@ def getFilePathDialog(self, searchDirectory, filter="dBASE (*.dbf)"):
         # TODO: gui -- provide enumeration similar to communicateWithUserInQgis for filter type. only permit dbf & shp
 
         # get full path to file
-        filepath,filter = QFileDialog.getOpenFileNameAndFilter(self.iface.mainWindow(),"Please choose a file to open...",
-                    searchDirectory,filter,"Filter list for selecting files from a dialog box")
+        filepath,filter = QFileDialog.getOpenFileNameAndFilter(iface.mainWindow(),"Please choose a file to open...", searchDirectory, filter, "Filter list for selecting files from a dialog box")
 
         if len(filepath) == 0:
             return None
         else:
-            self.communicateWithUserInQgis(filepath)
+            print "error"
+            #self.communicateWithUserInQgis(filepath)
             return filepath
 
 
@@ -32,3 +35,6 @@ def getUserSettings():
     """
 
     pass
+
+
+getFilePathDialog("/Users/drownedfrog/tmp")
